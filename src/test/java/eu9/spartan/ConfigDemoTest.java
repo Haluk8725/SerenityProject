9c129558017b57db7c62f35a3042ac68cdcfefd2
Haluk Ural <haluk8725@hotmail.com>
2022-12-12 13:02:33 +0300

    Last Change

---
+++ src/test/java/eu9/spartan/ConfigDemoTest.java
@@ -0,0 +1,13 @@
+package eu9.spartan;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import utilities.ConfigReader;
+@Disabled
+public class ConfigDemoTest {
+    @Test
+    public void test1(){
+        System.out.println(ConfigReader.getProperty("serenity.project.name"));
+        System.out.println(ConfigReader.getProperty("spartan.editor.username"));
+    }
+}
